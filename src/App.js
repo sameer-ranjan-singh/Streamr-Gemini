3e258b8c81909fe8ff020c499ff5058f32d27ca4
Sameer Ranjan Singh <sameer.lancer99@gmail.com>
2024-08-01 19:43:58 +0530

    CRA + tailwindConfig

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center bg-orange-400 p-2 m-2 ">
+     <span>Sameer Ranjan Singh</span>
+    </div>
+  );
+}
+
+export default App;
