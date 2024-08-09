cda510c5487525deea125e0afaae8757d5494c72
Sameer Ranjan Singh <sameer.lancer99@gmail.com>
2024-08-09 11:03:55 +0530

    fix : custom hook useNowpLayingMovies

--- src/features/browse/Browse.js
+++ src/features/browse/Browse.js
@@ -2,10 +2,10 @@ import React from "react";
 import MainComponent from "./components/MainComponent";
 import SecondaryComponent from "./components/SecondaryComponent";
 import Header from "../header/Header";
-import useNowPlayingMovies from "../../hooks/useNowPlayingMovies"
+import useNowPlayingMovies from "../../hooks/useNowPlayingMovies";
 
 const Browse = () => {
-  const movies = useNowPlayingMovies()
+  useNowPlayingMovies()
 
   return (
     <div>
